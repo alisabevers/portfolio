e212fc89627852839e7b165b4895875b7d2f2189
A Bevers <alisabevers@gmail.com>
2023-06-05 20:12:47 -0400

    created components files and tested to make sure something rendered when starting the app

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import Education from './Education';
+import Projects from './Projects';
+import Skills from './Skills';
+import Contact from './Contact';
+
+function AboutMe() {
+    console.log('i am connected woohoo');
+    return (
+        <div>hi i'm working!</div>
+    )
+}
+
+export default AboutMe;
