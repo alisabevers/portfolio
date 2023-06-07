684784a55482d24ea49187a8efffb66458474307
A Bevers <alisabevers@gmail.com>
2023-06-07 17:38:30 -0400

    created containers for projects. Used filler image and content for now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,54 @@
+import projectImg1 from '../Assets/PlaceholderProfile.jpeg'
+
 function Projects() {
     return (
         <div id="projectSection">
             <h1 id="projectsHeader">Projects</h1>
-            Projects will go here        
+            <div class="card projectsCard">
+                <div class="row justify-content-md-center">
+                    <div class="col projectImages">
+                        <img src={projectImg1} class="img-fluid rounded-start" alt="placeholder image for project 1" />
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Project title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            {/* WILL ADD PROJECT LINKS LATER */}
+                            <a href="#" target="_blank">View Project</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card projectsCard">
+                <div class="row justify-content-md-center">
+                    <div class="col projectImages">
+                        <img src={projectImg1} class="img-fluid rounded-start" alt="placeholder image for project 1" />
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Project title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            {/* WILL ADD PROJECT LINKS LATER */}
+                            <a href="#" target="_blank">View Project</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card projectsCard">
+                <div class="row justify-content-md-center">
+                    <div class="col projectImages">
+                        <img src={projectImg1} class="img-fluid rounded-start" alt="placeholder image for project 1" />
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Project title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            {/* WILL ADD PROJECT LINKS LATER */}
+                            <a href="#" target="_blank">View Project</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
